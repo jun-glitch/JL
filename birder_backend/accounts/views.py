567f2cf488a71d7ba906e53f36372360835594d6
최이준 <dlwns2076422@ewhain.net>
2026-01-12 14:17:29 +0900

    아이디,비번찾기,비번변경

--- birder_backend/accounts/views.py
+++ birder_backend/accounts/views.py
@@ -8,6 +8,15 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 # 회원가입 입력 검증용 Serializer
 from .serializers import SignupSerializer
 
+# 회원 테이블 접근용 User
+from django.contrib.auth.models import User
+
+# 비밀번호 변경 후 로그아웃 방지
+from django.contrib.auth import update_session_auth_hash
+
+# 가입 시 사용했던 비밀번호 유효성 검사
+from django.contrib.auth.password_validation import validate_password
+
 # 회원가입 API 뷰
 class SignupView(APIView):
     permission_classes = [AllowAny] # 누구나 접근 가능(로그인 전 사용자도 접근 가능해야 해서)
@@ -40,3 +49,67 @@ class MeView(APIView):
 
         # 로그인한 사용자 정보 반환
         return Response({"id": u.id, "username": u.username, "email": u.email})
+
+# 아이디 찾기
+class FindUsernameView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        email = request.data.get('email') # 이메일 data 변수명 email로 보내기
+
+        if not email: # 이메일을 입력하지 않은 경우 프론트에서 막아주기
+            return Response({"error" : "이메일을 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            user = User.objects.get(email=email)
+            return Response({"username" : user.username}, status=status.HTTP_200_OK)
+        
+        except User.DoesNotExist:
+            return Response({"error" : "해당 이메일로 가입된 아이디가 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+# 비밀번호 찾기
+class FindPwdView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        username = request.data.get('username') # id data 변수명 username
+        email = request.data.get('email') # data 변수명 email
+
+        if not username: # 아이디 입력 안 한 경우 프론트에서 막아주기
+            return  Response({"error" : "아이디를 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if not email: # 이메일을 입력 안 한 경우 프론트에서 막아주기
+            return  Response({"error" : "이메일을 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            user = User.objects.get(email=email, id=username)
+            
+        except User.DoesNotExist:
+            return Response({"error" : "해당 정보로 가입된 계정이 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+# 비밀번호 변경: 1. 로그인 중 변경 2. 비밀번호 찾기 이메일에서 랜딩
+class ChangePwdView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        user = request.user
+        old_pwd = request.data.get('old_pwd')
+        new_pwd = request.data.get('new_pwd')
+        new_pwd_confirm = request.data.get('new_pwd_confirm')
+
+        if not user.check_password(old_pwd):
+            return Response({"error" : "기존 비밀번호가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # 새로운 비밀번호 확인이 틀린 경우 > front에서 그냥 막아주면 좋을 듯
+        if new_pwd != new_pwd_confirm:
+            return Response({"error" : "새 비밀번호가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # 가입할 때 사용했던 비밀번호 유효성 검사
+        try:
+            validate_password(new_pwd, user)
+        except Exception as e:
+            return Response({"error" : list(e.messages)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user.set_password(new_pwd)
+        user.save()
+
+        update_session_auth_hash(request, user)
+
+        return Response({"message" : "비밀번호가 성공적으로 변경되었습니다."}, status=status.HTTP_200_OK)
